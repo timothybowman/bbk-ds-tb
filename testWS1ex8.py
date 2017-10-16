0f72c97d265a7668544b6b12f7035c1a9b771122
Tim Bowman <tbowma01@dcs.bbk.ac.uk>
2017-10-16 22:37:23 +0100

    test for ex 8

---
+++ testWS1ex8.py
@@ -0,0 +1,6 @@
+length_miles = 10 * 0.621371
+print(length_miles)
+minutes = 42.7
+#42 seconds is equivalent to .7 of a minute
+average_speed_miles_hour = length_miles / minutes
+print(average_speed_miles_hour)
